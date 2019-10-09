06ff71bf79056dc476e35233435d5d64c0ca30c7
Physopholy <56270157+Physopholy@users.noreply.github.com>
2019-10-09 11:59:05 -0500

    Add files via upload

--- gamma-phi.py
+++ gamma-phi.py
@@ -2,18 +2,27 @@ import numpy as np
 
 #just comment out inputs/routines not needed
 print('Enter guesses for unknown variables.')
-t=float(input("Enter T(degC):"))
+#t=float(input("Enter T(degC):"))
+t=45
 t=273+t
-p=float(input("Enter P(bar):"))
-x1=float(input("Enter x1:"))
+#p=float(input("Enter P(bar):"))
+p=1.5
+#x1=float(input("Enter x1:"))
+x1=.259
 x2=1-x1
-y1=float(input("Enter y1:"))
+#y1=float(input("Enter y1:"))
+y1=.75
 y2=1-y1
-psat1=float(input("Enter Psat1(bar):"))
-psat2=float(input("Enter Psat2(bar):"))
-b1=float(input("Enter b1 from binaryinteractions.py:"))
-b2=float(input("Enter b2 from binaryinteractions.py:"))
-b12=float(input("Enter b12 from binaryinteractions.py:"))
+#psat1=float(input("Enter Psat1(bar):"))
+psat1=2.34218
+#psat2=float(input("Enter Psat2(bar):"))
+psat2=.44265
+#b1=float(input("Enter b1 from binaryinteractions.py:"))
+b1=-609.83
+#b2=float(input("Enter b2 from binaryinteractions.py:"))
+b2=-765.94
+#b12=float(input("Enter b12 from binaryinteractions.py:"))
+b12=-680.65
 del12=2*b12-b1-b2
 
 
@@ -40,20 +49,25 @@ def gammaphi(y1,y2,p,t,x1,x2,g1,g2,psat1,psat2):
 
 	def deriv(y,dy,p,dp,phi,x,gamma,psat): #centered-difference approximation
 		f_0=obj(y-dy,p-dp,phi,x,gamma,psat)
+		#print('f_0=',f_0)
 		f_1=obj(y+dy,p+dp,phi,x,gamma,psat)
+		#print('f_1=',f_1)
 		dy_dx=(f_1-f_0)/(2*(dy+dp))
+		#print('dydx=',dy_dx)
 		return(dy_dx)
 
 	
 
 	phi1=phi_solver(b1,p,psat1,y2,del12,t) #these lines just initialize error
 	phi2=phi_solver(b2,p,psat2,y1,del12,t)
-	a=obj(y1,p,phi1,x1,g1,psat1)
-	b=obj(y2,p,phi2,x2,g2,psat2)
-	Fyp=np.array([[a],[b]])
+	Fyp=np.array([[obj(y1,p,phi1,x1,g1,psat1)],[obj(y2,p,phi2,x2,g2,psat2)]])
 	error=np.linalg.norm(Fyp)
+	index=0
+	x=np.array([[y1],[p]])
 
 	while error>0.000001:
+		index=index+1
+		print(index)
 		phi1=phi_solver(b1,p,psat1,y2,del12,t)
 		phi2=phi_solver(b2,p,psat2,y1,del12,t)
 
@@ -62,23 +76,30 @@ def gammaphi(y1,y2,p,t,x1,x2,g1,g2,psat1,psat2):
 		dobj2_dy1=deriv(y2,.00000001,p,0,phi2,x2,g2,psat2)
 		dobj2_dp=deriv(y2,0,p,.00000001,phi2,x2,g2,psat2)
 
-		x=np.array([[y1],[p]])
-		a=obj(y1,p,phi1,x1,g1,psat1)
-		b=obj(y2,p,phi2,x2,g2,psat2)
+		#x=np.array([[y1],[p]])
+		print('x',x)
 		Fyp=np.array([[obj(y1,p,phi1,x1,g1,psat1)],[obj(y2,p,phi2,x2,g2,psat2)]])
+		print('fyp',Fyp)
 		jacob=np.array([[dobj1_dy1,dobj1_dp],[dobj2_dy1,dobj2_dp]])
+		print('jacob',jacob)
 		jinv=np.linalg.inv(jacob)
+		print('jinv',jinv)
 
 		x=x-np.dot(jinv,Fyp)
+		print('x',x)
 
 		#start here, need to figure out how to slice numpy array x
 
 		y1=x[0]
+		print('y1',y1)
 		y2=1-y1
+		print('y2',y2)
 		p=x[1]
+		print('p',p)
 		error=np.linalg.norm(Fyp)
+		print('error',error)
 
-	return(p,t,y1,y2,phi1,phi2,x1,x2,g1,g2,psat1,psat2)
+	return(p,t,y1,y2,phi1,phi2,x1,x2,g1,g2,psat1,psat2,index)
 
 answer=gammaphi(y1,y2,p,t,x1,x2,g1,g2,psat1,psat2)
 p=answer[0]
@@ -106,4 +127,5 @@ print('gamma_1= ',g1)
 print('gamma_2= ',g2)
 print('Psat1(bar)= ',psat1)
 print('Psat2(bar)= ',psat2)
+print('Steps taken:',answer[12])
 
